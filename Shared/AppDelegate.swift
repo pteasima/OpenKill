8d6a53def638f49240f74ea1cb772f665007d387
Nistroj <serhij.nistor@gmail.com>
2022-02-24 20:37:31 +0100

    added AppDelegate class

---
+++ Shared/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  OpenKill
+//
+//  Created by Serhij Nistor on 24.02.2022.
+//
+
+import Firebase
+import UIKit
+
+// @UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions:
+                     [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
