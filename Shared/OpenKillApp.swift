32c50ff044ffefaf23495457cd4c61378460dfc3
Petr Sima <ptea.sima@gmail.com>
2022-02-24 15:58:25 +0100

    Initial Commit

---
+++ Shared/OpenKillApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenKillApp.swift
+//  Shared
+//
+//  Created by Petr Šíma on 24.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct OpenKillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
